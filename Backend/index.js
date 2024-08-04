5297cfb674f61afcc69b3d74447a3f2b1f535738
Abhishek Sharma <abhi2022.as@gmail.com>
2024-08-04 16:12:29 +0530

    PORT Update

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const authRoutes = require('./routes/Auth');
 const dbConnect = require('./config/database');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 // --------------------------- Middlewares ------------------------------------------
 const app = express()
@@ -10,13 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
-
 app.use('/api/phase1/auth', authRoutes);
 
 //------------------------------------------------------------------------------------
-
+const PORT = process.env.PORT || 5000;
 dbConnect();
 
-app.listen(4000,() => {
-    console.log('listen on 4000');
+app.listen(PORT,() => {
+    console.log(`listen on ${PORT}`);
 })
