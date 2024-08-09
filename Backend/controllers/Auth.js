7b9ba9ab309021e50588ab7e81d2f71efc1ae302
Abhishek Sharma <abhi2022.as@gmail.com>
2024-08-10 02:25:36 +0530

    Saving Cookies

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -192,7 +192,9 @@ exports.signIn = async (req, res) => {
 
             const options = {
                 expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                httpOnly: true,
+                httpOnly: false,
+                secure : true,
+                sameSite : "none"
             }
             res.cookie("token", token, options).status(200).json({
                 success: true,
